e2b114b873b6bd0e926ee18eade1e1c280cc02f4
Matt Deuschle <mdeuschle@gmail.com>
2018-09-27 21:54:45 -0700

    input accessory view

--- shopper/Controller/MainVC.swift
+++ shopper/Controller/MainVC.swift
@@ -10,8 +10,31 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet private weak var currencyTextField: UITextField!
+    @IBOutlet private weak var itemTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpCalculateButton()
+    }
+
+    private func setUpCalculateButton() {
+        let button = UIButton(frame: CGRect(x: 0,
+                                            y: 0,
+                                            width: view.bounds.width,
+                                            height: 60))
+        button.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+        button.setTitle("Calculate", for: .normal)
+        button.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        button.addTarget(self,
+                         action: #selector(buttonTapped),
+                         for: .touchUpInside)
+        currencyTextField.inputAccessoryView = button
+        itemTextField.inputAccessoryView = button
+    }
+
+    @objc private func buttonTapped() {
+        print("TAP")
     }
 }
 
--- shopper/View/CurrencyTextField.swift
+++ shopper/View/CurrencyTextField.swift
@@ -8,18 +8,65 @@
 
 import UIKit
 
+@IBDesignable
 class CurrencyTextField: UITextField {
+
+    override func prepareForInterfaceBuilder() {
+        setupView()
+    }
+
+
     override func awakeFromNib() {
         super.awakeFromNib()
-        backgroundColor = #colorLiteral(red: 0, green: 0.5690457821, blue: 0.5746168494, alpha: 0.2534081376)
+        setupView()
+    }
+
+    private func setupView() {
+        backgroundColor = #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1)
         layer.cornerRadius = 5.0
         textAlignment = .center
 
-        if let p = placeholder {
-            let _placeholder = NSAttributedString(string: p,
-                                                  attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
-            attributedPlaceholder = _placeholder
-            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        if let _placeholder = placeholder {
+            let attributedString = NSAttributedString(string: _placeholder,
+                                                      attributes: [.foregroundColor : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = attributedString
         }
+        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//class CurrencyTextField: UITextField {
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        backgroundColor = #colorLiteral(red: 0, green: 0.5690457821, blue: 0.5746168494, alpha: 0.2534081376)
+//        layer.cornerRadius = 5.0
+//        textAlignment = .center
+//
+//        if let p = placeholder {
+//            let _placeholder = NSAttributedString(string: p,
+//                                                  attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+//            attributedPlaceholder = _placeholder
+//            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+//        }
+//    }
+//}
